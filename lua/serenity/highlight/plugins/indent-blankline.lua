d917189ac65e2de7f6aaa788ecf7d110fd0bed3c
Wansmer <wansmer@gmail.com>
2023-09-30 10:20:37 +0300

    upgrade indent-blankline to v3

--- lua/serenity/highlight/plugins/indent-blankline.lua
+++ lua/serenity/highlight/plugins/indent-blankline.lua
@@ -1,7 +1,12 @@
 local c = require('serenity.colors')
 
 return {
+  -- For v2
   IndentBlanklineChar = { fg = c.fg.sand0 },
   IndentBlanklineContextChar = { fg = c.cursor_line_number },
   IndentBlanklineContextStart = { sp = c.cursor_line_number, underline = true },
+
+  -- For v3
+  IblIndent = { fg = c.fg.sand0 },
+  IblScope = { fg = c.cursor_line_number },
 }
