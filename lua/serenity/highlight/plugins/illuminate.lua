61ef2700a6664a593e368fe7391d36170eb3779e
Wansmer <wansmer@gmail.com>
2024-01-30 07:31:11 +0300

    update `illuminate` hls

--- lua/serenity/highlight/plugins/illuminate.lua
+++ lua/serenity/highlight/plugins/illuminate.lua
@@ -1,7 +1,7 @@
 local c = require('serenity.colors')
 
 return {
-  IlluminatedWordText = { bg = c.bg.dark3 },
-  IlluminatedWordRead = { bg = c.bg.dark3 },
-  IlluminatedWordWrite = { bg = c.bg.dark3 },
+  IlluminatedWordText = { bg = c.bg.dark6 },
+  IlluminatedWordRead = { bg = c.bg.dark6 },
+  IlluminatedWordWrite = { bg = c.bg.dark6 },
 }
